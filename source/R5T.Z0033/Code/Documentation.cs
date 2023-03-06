412778567b049dec24575b1e29261b73085dbdd7
davidcoats <david@getsafetycone.com>
2023-03-06 00:25:34 -0800

    Initial commit.

---
+++ source/R5T.Z0033/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0033
+{
+	/// <summary>
+	/// .NET blogger links.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0033/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Z0033
+.NET blogger links.
